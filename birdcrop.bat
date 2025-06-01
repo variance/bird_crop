b33839af5e5fa3e4dd01071bd8ae7c0052c34acc
variance <mkamm@gmx.net>
2025-06-01 12:40:06 +0200

    add usage comment

--- birdcrop.bat
+++ birdcrop.bat
@@ -1,3 +1,7 @@
+REM -----------------------------------------------------------------------------
+REM This batch file is intended for drag & drop use: drop an image folder or file onto it.
+REM Adjust the PYTHON executable path and the run_birdcrop.py script path below for your system!
+REM -----------------------------------------------------------------------------
 @echo off
 echo Got argument "%~1"
 "%USERPROFILE%\Programme\WPy64-31160\python-3.11.6.amd64\python.exe" "%USERPROFILE%\Documents\Repositories\bird_crop\run_birdcrop.py" "%~1"
